466a402311c8a285bf45a2425dfb114c7103b593
funnyname05 <gerardocarrera098@gmail.com>
2026-05-18 00:59:52 -0400

    fixed connection

--- front_despacho/src/componentes/CrudAdmin/TableDespachos.jsx
+++ front_despacho/src/componentes/CrudAdmin/TableDespachos.jsx
@@ -2,13 +2,14 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Modal } from "./Modal";
 import { FormCierreDespacho } from "./FormCierreDespacho";
+import { API_DESPACHO } from "../../config/api";
 
 export const TableDespachos = () => {
   const [despachos, setDespachos] = useState([]);
 
   const despacho = async () => {
     await axios
-      .get("http://192.168.3.20/api/v1/despachos", {
+      .get(`${API_DESPACHO}/api/v1/despachos`, {
         headers:{
               'Content-Type': 'application/json',
               'Accept': 'application/json'
---
+++ front_despacho/src/config/api.js
@@ -0,0 +1,4 @@
+const API_DESPACHO = import.meta.env.VITE_API_DESPACHO_URL || "http://localhost:8081"
+const API_VENTAS = import.meta.env.VITE_API_VENTAS_URL || "http://localhost:8080"
+
+export { API_DESPACHO, API_VENTAS }
