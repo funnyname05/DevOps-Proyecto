b63067d4b605d479d419965ae87bb132a915d1dc
funnyname05 <gerardocarrera098@gmail.com>
2026-05-17 20:16:13 -0400

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,8 @@
 CREATE USER IF NOT EXISTS 'user_ventas'@'%' IDENTIFIED BY 'password_ventas';
 CREATE USER IF NOT EXISTS 'user_despachos'@'%' IDENTIFIED BY 'password_despachos';
 
+-- Comando Test
+
 -- Base de datos de Ventas
 CREATE DATABASE IF NOT EXISTS db_ventas;
 USE db_ventas;
